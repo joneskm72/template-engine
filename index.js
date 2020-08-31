66075c9390d90391afe2a88c4870787ba17c293e
Kasey Jones <joneskm72@gmail.com>
2020-08-31 14:02:13 -0400

    Added question arrays

---
+++ index.js
@@ -0,0 +1,68 @@
+const manager = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
+    },
+    {
+        type: "input",
+        name: "office number",
+        message: "Please enter your office number"
+    }
+];
+
+const engineer = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username"
+    }
+];
+
+const intern = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Please enter the name of your school"
+    }
+];
